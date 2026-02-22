3bf50a8279d6a2d592231cba575ce638f9dfb5b5
SakhAlgo <sakhaglo@gmail.com>
2026-02-22 18:19:52 +1100

    other bad code

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -8,11 +8,10 @@ const ideas = [
 
 const trpc = initTRPC.create()
 
-const x: number = 'hello'
+const x: number = 'hello123'
 
-if (true) {
-  console.log(123)
-}
+if (true)  console.log(123)
+
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
