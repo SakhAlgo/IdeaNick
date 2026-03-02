ae7f7876578c2c7c113935d6ff4a680bffdc567e
SakhAlgo <sakhaglo@gmail.com>
2026-03-03 09:48:35 +1100

    Lesson 40

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,3 +1,4 @@
+import { zCreateIdeaTrpcInput } from '@ideanick/backend/src/router/createIdea/input'
 import { useFormik } from 'formik'
 import { z } from 'zod'
 import { Input } from '../../components/Input'
@@ -5,15 +6,7 @@ import { Segment } from '../../components/Segment'
 import { TextArea } from '../../components/TextArea'
 import { trpc } from '../../lib/trpc'
 
-const schema = z.object({
-  name: z.string().min(1),
-  nick: z
-    .string()
-    .min(1)
-    .regex(/^[a-z0-9-]+$/, 'Nick may contain only lowercase letters, numbers and dashes'),
-  description: z.string().min(1),
-  text: z.string().min(100, 'Text should be at least 100 characters long'),
-})
+const schema = zCreateIdeaTrpcInput
 
 type FormValues = z.infer<typeof schema>
 
