c23ecb9d1a1012b9f15cbd05fcf4537fa17d68ea
SakhAlgo <sakhaglo@gmail.com>
2026-03-12 16:27:08 +1100

    Lesson 69

--- webapp/src/components/Button/index.tsx
+++ webapp/src/components/Button/index.tsx
@@ -1,10 +1,17 @@
 import cn from 'classnames'
+import { Link } from 'react-router-dom'
 import css from './index.module.scss'
-
 export const Button = ({ children, loading = false }: { children: React.ReactNode; loading?: boolean }) => {
   return (
     <button className={cn({ [css.button]: true, [css.loading]: loading })} type="submit" disabled={loading}>
       {loading ? 'Loading...' : children}
     </button>
+  )
+}
+export const LinkButton = ({ children, to }: { children: React.ReactNode; to: string }) => {
+  return (
+    <Link className={cn({ [css.button]: true })} to={to}>
+      {children}
+    </Link>
   )
 }
