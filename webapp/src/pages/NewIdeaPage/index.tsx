9ecf32d9387050fa4a8aed471ffb60e830e615fc
SakhAlgo <sakhaglo@gmail.com>
2026-03-13 20:21:44 +1100

    Lesson 70

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,58 +1,29 @@
 import { zCreateIdeaTrpcInput } from '@ideanick/backend/src/router/createIdea/input'
-import { useFormik } from 'formik'
-import { useState } from 'react'
-import { z } from 'zod'
 import { Alert } from '../../components/Alert'
 import { Button } from '../../components/Button'
 import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { TextArea } from '../../components/TextArea'
+import { useForm } from '../../lib/form'
 import { trpc } from '../../lib/trpc'
 
-const schema = zCreateIdeaTrpcInput
-
-type FormValues = z.infer<typeof schema>
-
 export const NewIdeaPage = () => {
-  const [successMessageVisible, setSuccessMessageVisible] = useState(false)
-  const [submitingError, setSubmitingError] = useState<string | null>(null)
   const createIdea = trpc.createIdea.useMutation()
-  const formik = useFormik<FormValues>({
+  const { formik, buttonProps, alertProps } = useForm({
     initialValues: {
       name: '',
       nick: '',
       description: '',
       text: '',
     },
-    validate: (values) => {
-      const result = schema.safeParse(values)
-      if (result.success) {
-        return {}
-      }
-      return result.error.issues.reduce(
-        (acc, issue) => {
-          const key = issue.path[0] as string
-          acc[key] = issue.message
-          return acc
-        },
-        {} as Record<string, string>
-      )
-    },
+    validationSchema: zCreateIdeaTrpcInput,
     onSubmit: async (values) => {
-      setSubmitingError(null)
-      try {
-        await createIdea.mutateAsync(values)
-        formik.resetForm()
-        setSuccessMessageVisible(true)
-        setTimeout(() => {
-          setSuccessMessageVisible(false)
-        }, 3000)
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        setSubmitingError(error.message)
-      }
+      await createIdea.mutateAsync(values)
+      formik.resetForm()
     },
+    successMessage: 'Idea created!',
+    showValidationAlert: true,
   })
 
   return (
@@ -63,10 +34,8 @@ export const NewIdeaPage = () => {
           <Input name="nick" label="Nick" formik={formik} />
           <Input name="description" label="Description" formik={formik} maxWidth={500} />
           <TextArea name="text" label="Text" formik={formik} />
-          {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
-          {!!submitingError && <Alert color="red">{submitingError}</Alert>}
-          {successMessageVisible && <Alert color="green">Idea created successfully!</Alert>}
-          <Button loading={formik.isSubmitting}>Create Idea</Button>
+          <Alert {...alertProps} />
+          <Button {...buttonProps}>Create Idea</Button>
         </FormItems>
       </form>
     </Segment>
--- webapp/src/pages/SignUpPage/index.tsx
+++ webapp/src/pages/SignUpPage/index.tsx
@@ -1,8 +1,5 @@
 import { zSignUpTrpcInput } from '@ideanick/backend/src/router/signUp/input'
-import { useFormik } from 'formik'
-import { withZodSchema } from 'formik-validator-zod'
-import Cookie from 'js-cookie'
-import { useState } from 'react'
+import Cookies from 'js-cookie'
 import { useNavigate } from 'react-router-dom'
 import { z } from 'zod'
 import { Alert } from '../../components/Alert'
@@ -10,48 +7,41 @@ import { Button } from '../../components/Button'
 import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
+import { useForm } from '../../lib/form'
 import { getAllIdeasRoute } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
-
-const formSchema = zSignUpTrpcInput.extend({ passwordAgain: z.string().min(1) }).superRefine((values, ctx) => {
-  if (values.password !== values.passwordAgain) {
-    ctx.addIssue({
-      code: z.ZodIssueCode.custom,
-      message: 'Passwords do not match',
-      path: ['passwordAgain'],
-    })
-  }
-})
-
-type FormValues = z.infer<typeof formSchema>
 
 export const SignUpPage = () => {
   const navigate = useNavigate()
   const trpcUtils = trpc.useContext()
-  const [submitingError, setSubmitingError] = useState<string | null>(null)
   const signUp = trpc.signUp.useMutation()
-  const formik = useFormik<FormValues>({
+  const { formik, buttonProps, alertProps } = useForm({
     initialValues: {
       nick: '',
       password: '',
       passwordAgain: '',
     },
-    validate: withZodSchema(formSchema),
+    validationSchema: zSignUpTrpcInput
+      .extend({
+        passwordAgain: z.string().min(1),
+      })
+      .superRefine((val, ctx) => {
+        if (val.password !== val.passwordAgain) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: 'Passwords must be the same',
+            path: ['passwordAgain'],
+          })
+        }
+      }),
     onSubmit: async (values) => {
-      try {
-        setSubmitingError(null)
-        const { token } = await signUp.mutateAsync(values)
-        Cookie.set('token', token, { expires: 99900 })
-        trpcUtils.invalidate()
-        navigate(getAllIdeasRoute())
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      } catch (error: any) {
-        setSubmitingError(error.message)
-      }
+      const { token } = await signUp.mutateAsync(values)
+      Cookies.set('token', token, { expires: 99999 })
+      void trpcUtils.invalidate()
+      navigate(getAllIdeasRoute())
     },
+    resetOnSuccess: false,
   })
-
   return (
     <Segment title="Sign Up">
       <form onSubmit={formik.handleSubmit}>
@@ -59,9 +49,8 @@ export const SignUpPage = () => {
           <Input name="nick" label="Nick" formik={formik} />
           <Input name="password" label="Password" type="password" formik={formik} maxWidth={500} />
           <Input name="passwordAgain" label="Password again" type="password" formik={formik} maxWidth={500} />
-          {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
-          {!!submitingError && <Alert color="red">{submitingError}</Alert>}
-          <Button loading={formik.isSubmitting}>Sign Up</Button>
+          <Alert {...alertProps} />
+          <Button {...buttonProps}>Sign Up</Button>
         </FormItems>
       </form>
     </Segment>
