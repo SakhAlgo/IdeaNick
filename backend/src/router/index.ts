550fae28c4638ca75dcd3decd776b760888d8e0d
SakhAlgo <sakhaglo@gmail.com>
2026-03-17 10:15:49 +1100

    Lesson 74

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,13 +1,13 @@
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
 import { trpc } from '../lib/trpc'
 // @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
-import { createIdeaTrpcRoute } from './createIdea'
-import { getIdeaTrpcRoute } from './getIdea'
-import { getIdeasTrpcRoute } from './getIdeas'
-import { getMeTrpcRoute } from './getMe'
-import { signInTrpcRoute } from './signIn'
-import { signUpTrpcRoute } from './signUp'
-import { updateIdeaTrpcRoute } from './updateIdea'
+import { signInTrpcRoute } from './auth/signIn'
+import { signUpTrpcRoute } from './auth/signUp'
+import { createIdeaTrpcRoute } from './ideas/createIdea'
+import { getIdeaTrpcRoute } from './ideas/getIdea'
+import { getIdeasTrpcRoute } from './ideas/getIdeas'
+import { updateIdeaTrpcRoute } from './ideas/updateIdea'
+import { getMeTrpcRoute } from './other/getMe'
 // @endindex
 
 export const trpcRouter = trpc.router({
---
+++ webapp/src/pages/ideas/AllIdeasPage/index.tsx
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom'
+import { Segment } from '../../../components/Segment'
+import { getViewIdeaRoute } from '../../../lib/routes'
+import { trpc } from '../../../lib/trpc'
+import css from './index.module.scss'
+
+export const AllIdeasPage = () => {
+  // const result = trpc.getIdeas.useQuery()
+  const { data, isLoading, isError, isFetched } = trpc.getIdeas.useQuery({})
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (isError) {
+    return <div>Error: {isError}</div>
+  }
+  if (!isFetched) {
+    return <div>Not fetched</div>
+  }
+
+  return (
+    <Segment title="All Ideas">
+      <div className={css.ideas}>
+        {data?.ideas?.map((idea) => (
+          <div className={css.idea} key={idea.nick}>
+            <Segment
+              size={2}
+              title={
+                <Link className={css.ideaLink} to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+                  {idea.name}
+                </Link>
+              }
+              description={idea.description}
+            />
+          </div>
+        ))}
+      </div>
+    </Segment>
+  )
+}
