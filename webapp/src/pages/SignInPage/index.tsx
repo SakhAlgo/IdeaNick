8a996ef29269469d458b7be1ab3625efffe032dc
SakhAlgo <sakhaglo@gmail.com>
2026-03-16 21:43:05 +1100

    Lesson 73

--- webapp/src/pages/SignInPage/index.tsx
+++ webapp/src/pages/SignInPage/index.tsx
@@ -7,10 +7,13 @@ import { FormItems } from '../../components/FormItems'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
 import { useForm } from '../../lib/form'
+import { withPageWrapper } from '../../lib/pageWrapper'
 import { getAllIdeasRoute } from '../../lib/routes'
 import { trpc } from '../../lib/trpc'
 
-export const SignInPage = () => {
+export const SignInPage = withPageWrapper({
+  redirectAuthorized: true,
+})(() => {
   const navigate = useNavigate()
   const trpcUtils = trpc.useUtils()
   const signIn = trpc.signIn.useMutation()
@@ -40,4 +43,4 @@ export const SignInPage = () => {
       </form>
     </Segment>
   )
-}
+})
