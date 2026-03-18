002f4225d2af0a2a722899fe7b2b3a2d7af81ca9
SakhAlgo <sakhaglo@gmail.com>
2026-03-19 10:24:32 +1100

    Lesson 83

--- backend/src/router/ideas/getIdeas/index.ts
+++ backend/src/router/ideas/getIdeas/index.ts
@@ -3,6 +3,9 @@ import { trpc } from '../../../lib/trpc'
 import { zGetIdeasTrpcInput } from './input'
 
 export const getIdeasTrpcRoute = trpc.procedure.input(zGetIdeasTrpcInput).query(async ({ ctx, input }) => {
+  // const normalizedSearch = input.search ? input.search.trim().replace(/[\s\n\t]/g, '_') : undefined
+  // const normalizedSearch = input.search ? input.search.trim().replace(/[\s\n\t]/g, ' & ') : undefined
+
   const rawIdeas = await ctx.prisma.idea.findMany({
     select: {
       id: true,
@@ -17,6 +20,17 @@ export const getIdeasTrpcRoute = trpc.procedure.input(zGetIdeasTrpcInput).query(
         },
       },
     },
+    where: !input.search
+      ? undefined
+      : {
+          OR: [
+            { name: { contains: input.search, mode: 'insensitive' } },
+
+            { description: { contains: input.search, mode: 'insensitive' } },
+
+            { text: { contains: input.search, mode: 'insensitive' } },
+          ],
+        },
     orderBy: [
       {
         createdAt: 'desc',
--- webapp/src/pages/ideas/AllIdeasPage/index.tsx
+++ webapp/src/pages/ideas/AllIdeasPage/index.tsx
@@ -1,30 +1,46 @@
+import { zGetIdeasTrpcInput } from '@ideanick/backend/src/router/ideas/getIdeas/input'
 import InfiniteScroll from 'react-infinite-scroller'
 import { Link } from 'react-router-dom'
+import { useDebounceValue } from 'usehooks-ts'
 import { Alert } from '../../../components/Alert'
+import { Input } from '../../../components/Input'
 import { layoutContentElRef } from '../../../components/Layout'
 import { Loader } from '../../../components/Loader'
 import { Segment } from '../../../components/Segment'
+import { useForm } from '../../../lib/form'
 import { getViewIdeaRoute } from '../../../lib/routes'
 import { trpc } from '../../../lib/trpc'
 import css from './index.module.scss'
 
 export const AllIdeasPage = () => {
+  const { formik } = useForm({
+    initialValues: { search: '' },
+    validationSchema: zGetIdeasTrpcInput.pick({ search: true }),
+  })
+  const [debouncedValue] = useDebounceValue(formik.values.search, 300)
+
   const { data, error, isLoading, isError, hasNextPage, fetchNextPage, isFetchingNextPage, isRefetching } =
     trpc.getIdeas.useInfiniteQuery(
-      {},
+      {
+        search: debouncedValue,
+      },
       {
         getNextPageParam: (lastPage) => {
           return lastPage.nextCursor
         },
       }
     )
-
   return (
     <Segment title="All Ideas">
+      <div className={css.filter}>
+        <Input maxWidth={'100%'} label="Search" name="search" formik={formik} />
+      </div>
       {isLoading || isRefetching ? (
         <Loader type="section" />
       ) : isError ? (
         <Alert color="red">{error.message}</Alert>
+      ) : !data || !data.pages[0].ideas.length ? (
+        <Alert color="brown">Nothing found by search</Alert>
       ) : (
         <div className={css.ideas}>
           <InfiniteScroll
@@ -41,7 +57,9 @@ export const AllIdeasPage = () => {
               </div>
             }
             getScrollParent={() => layoutContentElRef.current}
-            useWindow={(layoutContentElRef.current && getComputedStyle(layoutContentElRef.current).overflow) !== 'auto'}
+            useWindow={
+              layoutContentElRef.current == null || getComputedStyle(layoutContentElRef.current).overflow !== 'auto'
+            }
           >
             {data?.pages
               .flatMap((page) => page.ideas)
