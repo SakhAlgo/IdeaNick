f038157781e15f98dc18d5f91b0c6c3218a19e78
SakhAlgo <sakhaglo@gmail.com>
2026-03-24 12:28:40 +1100

    Lesson 92

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -10,6 +10,7 @@ const zEnv = z.object({
   NODE_ENV: z.string().trim().min(1).default('development'),
   PASSWORD_SALT: z.string().trim().min(1),
   INITIAL_ADMIN_PASSWORD: z.string().trim().min(1),
+  WEBAPP_URL: z.string().trim().min(1),
 })
 
 // eslint-disable-next-line node/no-process-env
