2358bab8d7ddf3860459435a6050e7d97fec5612
SakhAlgo <sakhaglo@gmail.com>
2026-03-25 21:25:43 +1100

    Lesson 98

--- webapp/src/lib/routes.ts
+++ webapp/src/lib/routes.ts
@@ -1,17 +1,1 @@
-const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
-  return Object.keys(object).reduce((acc, key) => ({ ...acc, [key]: `:${key}` }), {}) as Record<keyof T, string>
-}
-
-export const getAllIdeasRoute = () => '/'
-export const viewIdeaRouteParams = getRouteParams({ ideaNick: true })
-export type ViewIdeaRouteParams = typeof viewIdeaRouteParams
-export const getNewIdeaRoute = () => '/ideas/new'
-export const getViewIdeaRoute = ({ ideaNick }: ViewIdeaRouteParams) => `/ideas/${ideaNick}`
-export const getSignUpRoute = () => '/signup'
-export const getSignInRoute = () => '/signin'
-export const getEditProfileRoute = () => '/edit-profile'
-export const getSignOutRoute = () => '/signout'
-
-export const editIdeaRouteParams = getRouteParams({ ideaNick: true })
-export type EditIdeaRouteParams = typeof viewIdeaRouteParams
-export const getEditIdeaRoute = ({ ideaNick }: EditIdeaRouteParams) => `/ideas/${ideaNick}/edit`
+export * from '@ideanick/shared/routes'
