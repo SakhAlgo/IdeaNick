aca6c554c13eac5e820f09ca539e9753cc533801
SakhAlgo <sakhaglo@gmail.com>
2026-03-25 21:25:43 +1100

    Lesson 98

---
+++ shared/src/routes.ts
@@ -0,0 +1,17 @@
+const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
+  return Object.keys(object).reduce((acc, key) => ({ ...acc, [key]: `:${key}` }), {}) as Record<keyof T, string>
+}
+
+export const getAllIdeasRoute = () => '/'
+export const viewIdeaRouteParams = getRouteParams({ ideaNick: true })
+export type ViewIdeaRouteParams = typeof viewIdeaRouteParams
+export const getNewIdeaRoute = () => '/ideas/new-idea'
+export const getViewIdeaRoute = ({ ideaNick }: ViewIdeaRouteParams) => `/ideas/${ideaNick}`
+export const getSignUpRoute = () => '/signup'
+export const getSignInRoute = () => '/signin'
+export const getEditProfileRoute = () => '/edit-profile'
+export const getSignOutRoute = () => '/signout'
+
+export const editIdeaRouteParams = getRouteParams({ ideaNick: true })
+export type EditIdeaRouteParams = typeof viewIdeaRouteParams
+export const getEditIdeaRoute = ({ ideaNick }: EditIdeaRouteParams) => `/ideas/${ideaNick}/edit`
