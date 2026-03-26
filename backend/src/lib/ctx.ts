0eea5ffda488448f69975d69afd10524410c50da
SakhAlgo <sakhaglo@gmail.com>
2026-03-26 18:00:40 +1100

    Lesson 101

--- backend/src/lib/ctx.ts
+++ backend/src/lib/ctx.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const createAppContext = () => {
+  // eslint-disable-next-line node/no-process-env
   const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
   const prisma = new PrismaClient({ adapter })
   return {
